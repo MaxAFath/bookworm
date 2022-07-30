a725069f32775eada0704d47b9fe2d2be1c6f70c
Max Fathauer <maxfathauer@gmail.com>
2022-07-30 19:47:16 -0400

    test

--- server/server.js
+++ server/server.js
@@ -13,32 +13,30 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const startServer =async () =>{
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context:authMiddleware
-  })
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
 
   server.applyMiddleware({app});
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
 
-  console.log('Use graphQl at http://localhost:${PORT}${server.graphqlPath}')
+  /*app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });*/
+
+  db.once("open", () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
 }
 
-startServer();
 
-
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer(typeDefs, resolvers);
